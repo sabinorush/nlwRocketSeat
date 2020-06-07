b19de757732b6cdff31940f1b2ed49ebb697990b
sabinorush <gu.sabino@hotmail.com>
2020-06-07 12:19:13 -0300

    Day 4

--- src/server.js
+++ src/server.js
@@ -1,25 +1,36 @@
 const express = require("express")
 const server = express()
 
+
 //configurar pasta publica
 server.use(express.static("public"))
 
 
+
+//utilizanod template negine
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+})
+
+
 // Configurar caminhos da minha aplicação
-// Página inicial
+// Página inicial   
 //req: Requisição
 //res: Resposta
 server.get("/", (req, res) => {
-    res.sendFile(__dirname + "/views/index.html")
+   return res.render("index.html")
 })
 
 server.get("/create-point", (req, res) => {
-    res.sendFile(__dirname + "/views/create-point.html")
+    return res.render("create-point.html")
 })
 
-server.get("/search-results", (req, res) => {
-    res.sendFile(__dirname + "/views/search-results.html")
+server.get("/search", (req, res) => {
+    return res.render("search-results.html")
 })
+
 
 // ligar o servidor //CTR + C INTERROMPE O SERVIDOR NO TERMINAL, ( USAR DENTRO DO TERMINAL !)
 server.listen(3000)
